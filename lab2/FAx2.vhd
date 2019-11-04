2a2080014368d440be59c47b2a3ea82ab4cd5b7d
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-11-04 20:51:02 +0300

    Remove commented code

--- lab2/FAx2.vhd
+++ lab2/FAx2.vhd
@@ -30,30 +30,9 @@ end Structural;
 
 architecture Behavioral of FAx2 is
 signal s0, c0: STD_LOGIC;
---signal a0_xor_b0,
---       s0,
---       up_and0,
---       down_and0,
---       c0,
---       a1_xor_b1,
---       s1,
---       up_and1,
---       down_and1: std_logic;
+
 begin
---     a0_xor_b0 <= a(0) xor b(0);
---     s0 <= a0_xor_b0 xor c_in;
---     up_and0 <= c_in and a0_xor_b0;
---     down_and0 <= a(0) and b(0);
---     c0 <= up_and0 or down_and0;
---	 
---     a1_xor_b1 <= a(1) xor b(1);
---     s1 <= a1_xor_b1 xor c0;
---     up_and1 <= c0 and a1_xor_b1;
---     down_and1 <= a(1) and b(1);
---	 
---     s(0) <= s0;
---     s(1) <= s1;  
---     c_out <= up_and1 or down_and1;
+
    s0 <= a(0) xor b(0) xor c_in;
    c0 <= (a(0) and b(0)) or (a(0) and c_in) or (b(0) and c_in);
    s(0) <= s0;

9f5c707ebc4c1a7d749dc06a0f7f12115ac43eac
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-11-10 21:17:58 +0300

    Implement JC

---
+++ lab4/JcGenerator.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity JcGenerator is
+    Generic ( N : integer := 8);
+    Port ( CLK : in  STD_LOGIC;
+           LS : in  STD_LOGIC;
+           RST : in  STD_LOGIC;
+           Pin : in  STD_LOGIC_VECTOR (0 to N-1);
+           Pout : out  STD_LOGIC_VECTOR (0 to N-1));
+end JcGenerator;
+
+architecture Behavioral of JcGenerator is
+   signal reg : STD_LOGIC_VECTOR (0 to N-1) 
+        := (others => '0');
+begin
+   Main : process (CLK, LS, RST, Pin)
+   begin
+      if RST = '1' then
+         reg <= (others => '0');
+      elsif rising_edge(CLK) then
+         if LS = '0' then
+            reg <= Pin;
+         else
+            reg <= not reg(N-1) & reg(0 to N-2);
+         end if;
+      end if;
+   end process;
+
+   Pout <= reg;
+end Behavioral;
+

c676ca7795a6763821575a72fb700811cbfa10b3
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-11-20 22:32:17 +0300

    Implement internal m-generator + tests

--- lab4/ExternalMGenerator.vhd
+++ lab4/ExternalMGenerator.vhd
@@ -12,32 +12,34 @@ architecture Behavioral of ExternalMGenerator is
    signal reg, data : STD_LOGIC_VECTOR ( 0 to conf'high-1 ) 
         := (others => '0');
 begin
-   Main : process (RST, CLK)
-   begin
-      if RST = '1' then
-         reg <= (others => '0');
-      elsif rising_edge(CLK) then
-         reg <= data;
-      end if;
-   end process;
+    Main : process (RST, CLK, data)
+    begin
+        if RST = '1' then
+            reg <= (others => '0');
+        elsif rising_edge(CLK) then
+            reg <= data;
+        end if;
+    end process;
 
-   DataProcess : process (reg)
-      variable result : STD_LOGIC;
-      constant zeros : STD_LOGIC_VECTOR (0 to conf'high-1) 
+    DataProcess : process (reg)
+        variable result : STD_LOGIC;
+        constant zeros : STD_LOGIC_VECTOR (0 to conf'high-1) 
             := (others => '0');
-   begin
-      if reg = zeros then
-         result := '1';
-      else
-         result := conf(conf'high) and reg(conf'high-1);
-         for i in conf'high-2 downto 0 loop
-            if conf(i+1) = '1' then
-               result := result xor reg(i);
-            end if;
-         end loop;
-      end if;
-      data <= result & reg(0 to conf'high-2);
-   end process;
+    begin
+        if reg = zeros then
+            result := '1';
+        else
+            result := conf(conf'high) and reg(conf'high-1);
+            
+            for i in conf'high-2 downto 0 loop
+                if conf(i+1) = '1' then
+                   result := result xor reg(i);
+                end if;
+            end loop;
+            
+        end if;
+        data <= result & reg(0 to conf'high-2);
+    end process;
    
-   Pout <= reg;
+    Pout <= reg;
 end Behavioral;
--- lab4/JcGeneratorTests.vhd
+++ lab4/JcGeneratorTests.vhd
@@ -10,52 +10,52 @@ ARCHITECTURE Behavioral OF JcGeneratorTests IS
     -- Component Declaration for the Unit Under Test (UUT)
     COMPONENT JcGenerator
     PORT(
-         CLK : IN  std_logic;
-         LS : IN  std_logic;
-         RST : IN  std_logic;
-         Pin : IN  std_logic_vector(0 to N-1);
-         Pout : OUT  std_logic_vector(0 to N-1)
+        CLK : IN  std_logic;
+        LS : IN  std_logic;
+        RST : IN  std_logic;
+        Pin : IN  std_logic_vector(0 to N-1);
+        Pout : OUT  std_logic_vector(0 to N-1)
         );
     END COMPONENT;
     
-   --Inputs
-   signal CLK : std_logic := '0';
-   signal LS : std_logic := '0';
-   signal RST : std_logic := '0';
-   signal Pin : std_logic_vector(0 to N-1) 
+    --Inputs
+    signal CLK : std_logic := '0';
+    signal LS : std_logic := '0';
+    signal RST : std_logic := '0';
+    signal Pin : std_logic_vector(0 to N-1) 
         := (others => '0');
 
- 	--Outputs
-   signal Pout : std_logic_vector(0 to N-1);
-   
-   -- Clock period definitions
-   constant CLK_period : time := 10 ns;
+    --Outputs
+    signal Pout : std_logic_vector(0 to N-1);
+
+    -- Clock period definitions
+    constant CLK_period : time := 10 ns;
    
 BEGIN
-   uut: JcGenerator PORT MAP (
-          CLK => CLK,
-          LS => LS,
-          RST => RST,
-          Pin => Pin,
-          Pout => Pout
+    uut: JcGenerator PORT MAP (
+        CLK => CLK,
+        LS => LS,
+        RST => RST,
+        Pin => Pin,
+        Pout => Pout
         );
 
-   
-   LS_process :process
-   begin
-      LS <= '0';
-      wait for CLK_period;
-      LS <= '1';
-      wait for CLK_period*N;
-      Pin <= Pin + "1";
-   end process;
 
-   CLK_process :process
-   begin
-		CLK <= '0';
-		wait for CLK_period/2;
-		CLK <= '1';
-		wait for CLK_period/2;
-   end process;
+    LS_process :process
+    begin
+        LS <= '0';
+        wait for CLK_period;
+        LS <= '1';
+        wait for CLK_period*N;
+        Pin <= Pin + "1";
+    end process;
+
+    CLK_process :process
+    begin
+        CLK <= '0';
+        wait for CLK_period/2;
+        CLK <= '1';
+        wait for CLK_period/2;
+    end process;
    
 END;
